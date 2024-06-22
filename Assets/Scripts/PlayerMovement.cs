38596375726d6fbcf757f80d2ac4b335545bb55b
JBXamin <146653426+JBXamin@users.noreply.github.com>
2024-06-22 22:45:07 +0530

    first

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float hI;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hI = Input.GetAxis("Horizontal");
+        move();
+    }
+
+    void move()
+    {
+        rb.velocity = new Vector3(hI * 13f, 0f, 0f);
+        if (rb.velocity.x!=0)
+        {
+            GetComponent<Animator>().SetBool("isWalking", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isWalking", false);
+        }
+    }
+}
